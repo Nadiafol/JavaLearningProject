7b43d9ad56ff6bf89996e709c8bfc8cd3d2e7217
NadiaFol <foltea.nadejda@hotmail.com>
2023-05-10 17:46:52 +0300

    Initial Commit – Added the Variables and Comments Task

---
+++ src/main/java/com/orange/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,37 @@
+package com.orange.variablesandcomments;
+
+public class VariabilesAndComments {
+    public static void main(String[] args) {
+        // Declare primitive data type variables together with their initializations
+
+        boolean falseBooleanVariable = false;
+        boolean trueBooleanVariable = true;
+        byte lowerLimitOfByteDataType = -128;
+        byte upperLimitOfByteDataType = 127;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        short lowerLimitShortDataType = -32_768;
+        short upperLimitShortDataType = 32_767;
+        int lowerLimitOfIntDataType = -2_147_483_648;
+        int upperLimitOfIntDataType = 2_147_483_647;
+        float lowerLimitOfFloatDataType = -3.4E+38f;
+        float upperLimitOfFloatDataType = 3.4E+38f;
+        long lowerLimitOfLongDataType = -9_223_372_036_854_775_808L;
+        long upperLimitOfLongDataType = 9_223_372_036_854_775_807L;
+        double lowerLimitOfDoubleDataType = -1.7E+308d;
+        double upperLimitOfDoubleDataType = 1.7E+308d;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
